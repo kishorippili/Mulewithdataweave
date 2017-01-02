51749e546db776d71cbc7acd555f1ec400d87ffb
kishore ippili <kishor.ippili@gmail.com>
2017-01-02 10:42:10 +0530

    Added new Project

---
+++ dataweave_transformation/src/main/java/com/mulesoft/com/UserInput.java
@@ -0,0 +1,44 @@
+package com.mulesoft.com;
+
+public class UserInput {
+	
+	private String type;
+	private String label;
+	private String author;
+	private String image;
+	private String price;
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getLabel() {
+		return label;
+	}
+	public void setLabel(String label) {
+		this.label = label;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getImage() {
+		return image;
+	}
+	public void setImage(String image) {
+		this.image = image;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+
+	
+	
+
+}
